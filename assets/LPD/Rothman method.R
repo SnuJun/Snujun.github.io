9b89392e69a81403f7f74ba7443c7a6370ad498e
Junwoo Lee <166775835+SnuJun@users.noreply.github.com>
2026-06-17 13:25:05 +0900

    Update Rothman method.R

--- assets/LPD/Rothman method.R
+++ assets/LPD/Rothman method.R
@@ -4,10 +4,8 @@ library(PDSCE)
 
 ftn.cov.Roth = function(datamat, thresseq, tol = 10^-7, MAXITER = 10000) {
   time1 = Sys.time() ;
-  roth = pdsoft.cv(x = datamat, lam.vec = thresseq, standard = FALSE,init="soft", nsplits = 5, n.tr = 4, tolout = tol, maxitout=MAXITER,quiet=TRUE) ;
+  roth=pdsoft.cv(x=datamat,lam.vec = thresseq,tolout = tol,maxitout=MAXITER,quiet=TRUE)
   time2 = difftime(Sys.time(), time1, units="secs") ;
   return(list(est = roth$sigma, iter = NA, tol = tol,
               thres = roth$best.lam, time = time2)) ;
 }
-
-
